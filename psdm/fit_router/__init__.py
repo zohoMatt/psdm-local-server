4dd9296941506d5a87c3d1afc1184a56ec46baf8
zohoMatt <mattzuo12@gmail.com>
2020-07-22 16:39:43 -0400

    feat: /kfit API ready.

--- psdm/fit_router/__init__.py
+++ psdm/fit_router/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from psdm.fit_router.psdm_fit import psdm_kfit
 
 fit_app = Blueprint('fit_router', __name__)
 
@@ -6,5 +7,19 @@ fit_app = Blueprint('fit_router', __name__)
 @fit_app.route('/test')
 def test():
     return {
-        'tested': True
+        'args': {k: v for k, v in request.args.items()}
+    }
+
+
+@fit_app.route('/kfit')
+def kfit():
+    fit_curve = psdm_kfit(**{k: v for k, v in request.args.items()})
+    params, x, y = fit_curve['params'], fit_curve['index'], fit_curve['values']
+    return {
+        'x': x,
+        'y': y,
+        'length': len(x),
+        'valid': len(x) == len(y),
+        'params': params,
+        'msg': ''
     }
