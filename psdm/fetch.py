1b1897ef1eb4a6c00b65db057fdfb0241420c494
zohoMatt <mattzuo12@gmail.com>
2020-07-22 15:26:00 -0400

    feat: Replace imports in sour files cloned.

--- psdm/fetch.py
+++ psdm/fetch.py
@@ -2,6 +2,9 @@ import json
 import requests
 from os import path
 
+OLD_IMPORT = 'from PSDM_functions import *'
+NEW_IMPORT = 'from psdm.fit_router.repo.PSDM_functions import *'
+
 
 def download_repo_files():
     env = path.dirname(__file__)
@@ -12,5 +15,5 @@ def download_repo_files():
             response = requests.get(files[name])
             response.encoding = 'utf-8'
             with open(path.join(env, './fit_router/repo/{0}'.format(name)), 'w') as file:
-                file.write(response.text)
+                file.write(response.text.replace(OLD_IMPORT, NEW_IMPORT))
                 print('>>> Succeed in downloading {0}'.format(name))
